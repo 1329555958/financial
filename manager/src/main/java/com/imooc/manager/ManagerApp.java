72e3c4205647a7e820e7697d64117afda21f8e49
skyding <1329555958@qq.com>
2017-09-02 10:16:53 +0800

    4.4

--- manager/src/main/java/com/imooc/manager/ManagerApp.java
+++ manager/src/main/java/com/imooc/manager/ManagerApp.java
@@ -1,8 +1,11 @@
 package com.imooc.manager;
 
+import com.imooc.swagger.EnableMySwagger;
+import com.imooc.swagger.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
 
 /**
  * 管理端启动类
