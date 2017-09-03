ee46220869f05297df199c174471d87cd201324c
skyding <1329555958@qq.com>
2017-09-03 17:55:46 +0800

    5.1

---
+++ api/src/main/java/com/imooc/api/ProductRpc.java
@@ -0,0 +1,30 @@
+package com.imooc.api;
+
+import com.googlecode.jsonrpc4j.JsonRpcService;
+import com.imooc.api.domain.ProductRpcReq;
+import com.imooc.entity.Product;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+/**
+ * 产品相关的rpc服务
+ */
+@JsonRpcService("rpc/products")
+public interface ProductRpc {
+    /**
+     * 查询多个产品
+     *
+     * @param req
+     * @return
+     */
+    List<Product> query(ProductRpcReq req);
+
+    /**
+     * 查询单个产品
+     *
+     * @param id
+     * @return
+     */
+    Product findOne(String id);
+}
---
+++ manager/src/main/java/com/imooc/manager/configuration/RpcConfiguration.java
@@ -0,0 +1,17 @@
+package com.imooc.manager.configuration;
+
+import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImplExporter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * rpc相关配置
+ */
+@Configuration
+public class RpcConfiguration {
+
+    @Bean
+    public AutoJsonRpcServiceImplExporter rpcServiceImplExporter(){
+        return new AutoJsonRpcServiceImplExporter();
+    }
+}
