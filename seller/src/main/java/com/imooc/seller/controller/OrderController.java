d8ed6c8b1fd7bc8c64a044da2070d8ae5acf793c
skyding <1329555958@qq.com>
2017-10-24 21:37:26 +0800

    7.2

--- seller/src/main/java/com/imooc/seller/controller/OrderController.java
+++ seller/src/main/java/com/imooc/seller/controller/OrderController.java
@@ -1,14 +1,13 @@
 package com.imooc.seller.controller;
 
 import com.imooc.entity.Order;
+import com.imooc.seller.params.OrderParam;
 import com.imooc.seller.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 订单相关
@@ -24,12 +23,14 @@ public class OrderController {
     /**
      * 下单
      *
-     * @param order
+     * @param param
      * @return
      */
     @RequestMapping(value = "/apply", method = RequestMethod.POST)
-    public Order apply(@RequestBody Order order) {
-        LOG.info("申购请求:{}", order);
+    public Order apply(@RequestHeader String authId,@RequestHeader String sign, @RequestBody OrderParam param) {
+        LOG.info("申购请求:{}", param);
+        Order order = new Order();
+        BeanUtils.copyProperties(param,order);
         order = orderService.apply(order);
         LOG.info("申购结果:{}", order);
         return order;
