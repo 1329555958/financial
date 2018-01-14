eda398aaae502def55b4ecb494f9d25b8b1f36dc
skyding <1329555958@qq.com>
2018-01-14 18:14:49 +0800

    3.3redo

--- manager/src/main/java/com/imooc/manager/error/ErrorConfiguration.java
+++ manager/src/main/java/com/imooc/manager/error/ErrorConfiguration.java
@@ -1,24 +1,23 @@
 package com.imooc.manager.error;
 
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.web.ErrorAttributes;
-import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
+import org.springframework.boot.autoconfigure.web.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 /**
- * 错误处理配置类
+ * 错误处理相关配置
  */
 @Configuration
 public class ErrorConfiguration {
-
     @Bean
-    public MyErrorController errorController(ErrorAttributes errorAttributes,ServerProperties serverProperties,
-                                             ObjectProvider<List<ErrorViewResolver>> errorViewResolversProvider){
-        return new MyErrorController(errorAttributes,serverProperties.getError(),errorViewResolversProvider.getIfAvailable());
-
+    public MyErrorController basicErrorController(ErrorAttributes errorAttributes,ServerProperties serverProperties,
+                                                  ObjectProvider<List<ErrorViewResolver>> errorViewResolversProvider) {
+        return new MyErrorController(errorAttributes, serverProperties.getError(),
+                errorViewResolversProvider.getIfAvailable());
     }
 }
--- manager/src/main/java/com/imooc/manager/error/MyErrorController.java
+++ manager/src/main/java/com/imooc/manager/error/MyErrorController.java
@@ -10,25 +10,37 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 自定义错误处理
+ * 自定义错误处理controller
  */
 public class MyErrorController extends BasicErrorController{
     public MyErrorController(ErrorAttributes errorAttributes, ErrorProperties errorProperties, List<ErrorViewResolver> errorViewResolvers) {
         super(errorAttributes, errorProperties, errorViewResolvers);
     }
-
+    /**
+     * {
+     x"timestamp": "2018-01-14 10:41:17",
+     x"status": 500,
+     x"error": "Internal Server Error",
+     x"exception": "java.lang.IllegalArgumentException",
+     "message": "编号不可为空",
+     x"path": "/manager/products"
+     + code
+     + canRetry
+     }
+     */
     @Override
     protected Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
         Map<String, Object> attrs = super.getErrorAttributes(request, includeStackTrace);
-        //删除无关属性
         attrs.remove("timestamp");
         attrs.remove("status");
         attrs.remove("error");
         attrs.remove("exception");
         attrs.remove("path");
-        ErrorType type = ErrorType.getByName(attrs.get("message").toString());
-        attrs.put("message",type.getMessage());
-        attrs.put("code",type.getCode());
+        String errorCode = (String) attrs.get("message");
+        ErrorEnum errorEnum = ErrorEnum.getByCode(errorCode);
+        attrs.put("message",errorEnum.getMessage());
+        attrs.put("code",errorEnum.getCode());
+        attrs.put("canRetry",errorEnum.isCanRetry());
         return attrs;
     }
 }
